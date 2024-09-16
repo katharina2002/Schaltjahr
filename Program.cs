e6e8d89edcac28f8fa24c7177b07ec89d05e9e41
Katharina <katharina@wolf-wide-web.de>
2024-09-16 22:12:43 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Schaltjahr ermitteln
+ */
+
+Console.WriteLine("Gebe ein Jahr ein");
+
+int jahr = Convert.ToInt32(Console.ReadLine());
+if (jahr % 4 == 0)
+{
+    if (jahr % 100 == 0)
+    {
+        if (jahr % 400 == 0)
+        {
+            Console.WriteLine($"{jahr} ist ein Schaltjahr");
+        }
+        else
+        {
+            Console.WriteLine($"{jahr} ist kein Schaltjahr");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"{jahr} ist ein Schaltjahr");
+    }
+}
+else
+{
+    Console.WriteLine($"{jahr} ist kein Schaltjahr");
+}
